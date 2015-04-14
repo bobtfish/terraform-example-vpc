d73b62c18267d2b6b3e6c71ed90479c74f130793
Tomas Doran <bobtfish@bobtfish.net>
2015-04-14 23:19:29 +0000

    Add a puppet client

--- eucentral1-demo/internal.tf
+++ eucentral1-demo/internal.tf
@@ -9,3 +9,14 @@ module "puppetmaster" {
   repository = "https://github.com/bobtfish/terraform-puppet.git"
 }
 
+module "puppet-client" {
+  source = "github.com/terraform-community-modules/tf_aws_puppet/agent"
+  region = "${var.region}"
+  instance_type = "t2.micro"
+  iam_instance_profile = "describe-instances"
+  aws_key_name = "${module.vpc.aws_key_name}"
+  subnet_id = "${element(split(\",\", module.vpc.dedicatedsubnets), 0)}"
+  security_group = "${module.vpc.security_group_allow_all}"
+  puppetmaster_ip = "${module.puppetmaster.private_ip}"
+}
+
