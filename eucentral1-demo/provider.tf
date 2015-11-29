2ad1d314e3215d572aa50c93e355fdc817033d62
Tomas Doran <tdoran@yelp.com>
2015-11-29 00:26:11 +0000

    Fix the provider

--- eucentral1-demo/provider.tf
+++ eucentral1-demo/provider.tf
@@ -1,10 +1,4 @@
 provider "aws" {
     region = "${var.region}"
-    credentials_file_profile = "${var.account}"
-    credentials_provider = "file"
-    security_token = ""
-    secret_key = ""
-    access_key = ""
-    credentials_file_path = ""
 }
 
